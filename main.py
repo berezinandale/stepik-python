ef909e963a482dd2df7ab65331c05c1c006bea62
berezinandale <berezinandale@gmail.com>
2025-03-23 14:30:29 +0300

    Больше предыдущего

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
-def count_points_in_quadrants(points):
+def count_larger_than_previous(numbers):
+    counter = 0
 
-    q1 = q2 = q3 = q4 = 0
+    for i in range(1, len(numbers)):
+        if numbers[i] > numbers[i - 1]:
+            counter += 1
 
-    for x, y in points:
-        if x > 0 and y > 0:
-            q1 += 1
-        if x < 0 and y > 0:
-            q2 += 1
-        if x < 0 and y < 0:
-            q3 += 1
-        if x > 0 and y < 0:
-            q4 += 1
-
-    return q1, q2, q3, q4
+    return counter
 
 def main():
-    n = int(input())
-    points = []
 
-    for _ in range(n):
-        x, y = map(int, input().split())
-        points.append((x, y))
+    input_str = input()
+    numbers = list(map(int, input_str.split()))
 
-    q1, q2, q3, q4 = count_points_in_quadrants(points)
+    result = count_larger_than_previous(numbers)
 
-    print("Первая четверть:", q1)
-    print("Вторая четверть:", q2)
-    print("Третья четверть:", q3)
-    print("Четвертая четверть:", q4)
+    print(result)
 
 if __name__ == "__main__":
     main()
+
+
+
 
